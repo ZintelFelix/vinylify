502a93421a341c88243899149138a173b2eae0cd
Felix Zintel <fezi1999@googlemail.com>
2024-03-16 16:36:58 +0100

    added descriptive labels

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -1,11 +1,9 @@
-
 import Link from "next/link";
 import CoverImage from "./cover-image";
 
 type Props = {
   title: string;
   coverImage: string;
-  excerpt: string;
   slug: string;
   artist: string;
   genre: string;
@@ -14,16 +12,29 @@ type Props = {
 
 export function PostPreview({
   title,
+  artist,
+  genre,
+  yor,
   coverImage,
-  excerpt,
   slug,
 }: Props) {
   return (
     <div>
       <div className="mb-1 border flex justify-end">
         <CoverImage slug={slug} title={title} src={coverImage} />
-        <div className="flex-grow font-bold"> 
-          {title}
+        <div className="flex-grow font-bold">
+          <div>
+            <span>Title:</span> {title}
+          </div>
+          <div>
+            <span>Artist:</span> {artist}
+          </div>
+          <div>
+            <span>Genre:</span> {genre}
+          </div>
+          <div>
+            <span>Year:</span> {yor}
+          </div>
         </div>
       </div>
       <h3 className="text-2xl leading-snug">
