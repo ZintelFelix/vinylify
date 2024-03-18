8b3c627d2939885961f1ec3f7f545e190f1b9035
ZintelFelix <fezi1999@googlemail.com>
2024-03-18 15:02:14 +0100

    added div to header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -2,12 +2,14 @@ import Link from "next/link";
 
 const Header = () => {
   return (
+    <div>
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/" className="hover:underline">
         Vinylify
       </Link>
       .
     </h2>
+    </div>
   );
 };
 
